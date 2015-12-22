f55a1380c9465ee8aa8db34a46d9022b2c78ea3e
Jordi Soler <jordi.solerbusquets@gmail.com>
2015-12-22 19:11:23 +0100

    Added proportional controller funtion and voctorized point treatement

--- prj_drone/src/prj_drone_node.cpp
+++ prj_drone/src/prj_drone_node.cpp
@@ -64,9 +64,11 @@ int RECEIVED_STATE_, CURRENT_STATE_;
 
 bool cam_, turning_, waiting_, performing_, waiting2_, ready2takeoff_;	// Indicates whether the bottom cam is  active or not.
 
-double _z_, _y_, _x_, yaw_;
-double desired_distance_z_=1, desired_distance_y_=0, desired_distance_x_=0, desired_angle_yaw_=1.57;
-double final_distance_z_=1, final_distance_y_=0, final_distance_x_=2;
+//double _z_, _y_, _x_, yaw_;
+double pose_ [3];
+double desired_distance [] = {0, 0, 1};
+double desired_angle_yaw_ = 1.57, yaw_;
+double final_distance [] = {2, 0, 1};
 
 ros::Time time_, prev_time_, t0_;
 
@@ -91,17 +93,48 @@ void tgcam(ros::ServiceClient camaras_sc)
 void inverseKinematics(double mx, double my, double mz)
 {
     if(cam_){
-	_x_ = mx;
-	_y_ = my;
-	_z_ = mz;
+	pose_[1] = mx;
+	pose_[0] = my;
+	pose_[2] = mz;
     }else{
-	_x_ = 3-mz;
-	_y_ = mx;
-	_z_ = 1+my;
-	ROS_INFO("Pose: x=%f, y=%f, z=%f", _x_,_y_,_z_);
+	pose_[0] = 3-mz;
+	pose_[1] = mx;
+	pose_[2] = 1+my;
+	ROS_INFO("Pose: x=%f, y=%f, z=%f", pose_[0],pose_[1],pose_[2]);
 	//ROS_INFO("Pose: x=%f, y=%f, z=%f", mx,my,mz);
     }
 }
+
+/*
+ *Proportional controller, guiven the desired goal, send the control action to reach it.
+ */
+double Pcontrol(double goal[], double gains[], int numel, ros::Publisher twist_pub)
+{
+    double err [numel];
+    double sqrErr=0;
+    geometry_msgs::Twist msg;
+
+    for(int i=0; i<numel; i++){
+    	if(i<3){
+	    err[i]=goal[i]-pose_[i];
+	}else{
+	    err[i]=goal[i]-yaw_;
+	}
+	sqrErr=sqrErr+pow(err[i],2);
+    }
+
+    msg.linear.x=gains[0]*err[0];
+    msg.linear.y=gains[1]*err[1];
+    msg.linear.z=gains[2]*err[2];
+
+    if(numel==4){
+    	msg.angular.z=gains[3]*err[3];
+    }
+    	
+    twist_pub.publish(msg);
+    return sqrt(sqrErr);
+}
+
 
 /*
  *Trajectory via point generator (Trajectory 1, straight line)
@@ -149,11 +182,11 @@ void dynrec_callback(prj_drone::dynamic_paramsConfig &config, uint32_t level)
 	actions_todo.update_angularZ=config.angularZ;
 
 	// Desired distances
-	desired_distance_z_=config.z_desitjada;
-	desired_distance_y_=config.y_desitjada;
-	desired_distance_x_=config.x_desitjada;
+	desired_distance[0]=config.x_desitjada;
+	desired_distance[1]=config.y_desitjada;
+	desired_distance[2]=config.z_desitjada;
 
-	ROS_INFO("Reconfigure Request. X: %f, Y: %f, Z: %f", desired_distance_x_, desired_distance_y_, desired_distance_z_);
+	ROS_INFO("Reconfigure Request. X: %f, Y: %f, Z: %f", desired_distance[0], desired_distance[1], desired_distance[2]);
 }
 
 void MarkerCallback(const ar_pose::ARMarkers::ConstPtr& message)
@@ -231,127 +264,111 @@ int main(int argc, char **argv)
 	// Main loop
 	while (ros::ok())
 	{
-		// Take off
-		if (actions_todo.update_takeoff && ready2takeoff_){
-			std_msgs::Empty msg;
-			chatter_pub.publish(msg);
-			ready2takeoff_=false;
-			ROS_INFO("I want to take off");
-		}
+	    // Take off
+	    if (actions_todo.update_takeoff && ready2takeoff_){
+		std_msgs::Empty msg;
+		chatter_pub.publish(msg);
+		ready2takeoff_=false;
+		ROS_INFO("I want to take off");
+	    }
 
-		// Land
-		if (actions_todo.update_land){
-			std_msgs::Empty msgL;
-			Land_pub.publish(msgL);
-		}
+	    // Land
+	    if (actions_todo.update_land){
+		std_msgs::Empty msgL;
+		Land_pub.publish(msgL);
+	    }
 
-		// Toggle camera
-		if (actions_todo.update_toggle_cam){
+	    // Toggle camera
+	    if (actions_todo.update_toggle_cam){
+		tgcam(camaras_sol);
+		actions_todo.update_toggle_cam=false;
+	    }
+
+	    // Reset ArDrone
+	    if (actions_todo.update_reset){
+		ROS_INFO("Reset!");
+		std_msgs::Empty msg;
+		reset_pub.publish(msg);
+		actions_todo.update_reset=false;
+	    }
+
+
+	    // State transitions
+	    if(CURRENT_STATE_ != RECEIVED_STATE_){
+		std::string state;
+
+		switch(RECEIVED_STATE_){
+		    case TAKINGOFF :
+			state = "Taking off";
+			if(!cam_){
+				tgcam(camaras_sol);
+			}
+			break;
+		    case FLYING :
+			if(CURRENT_STATE_==TAKINGOFF){
+			    turning_ = true;
+			    prev_time_ = time_;
+			}
+			state = "Hovering";
+			break;
+		    case LANDING :
+			state = "Landing";
+			break;
+		    case LANDED :
+			state = "Landed";
+			break;
+		}
+		ROS_INFO("STATE: %s", state.c_str());
+
+		CURRENT_STATE_ = RECEIVED_STATE_;
+	    }
+
+
+	    if(CURRENT_STATE_==FLYING){
+		if (turning_){
+		    double Ks []={1,1,1,1};
+		    double gs [4];
+		    for(int i=0; i<3; i++){
+			gs[i]=desired_distance[i];
+		    }
+		    gs[3] = desired_angle_yaw_;
+		    double err = Pcontrol(gs, Ks, 4, twist_pub);
+
+		    if(ros::Time::now()-prev_time_>TIME_TURNING_){
+			turning_  = false;
+			waiting_ = true;
+			prev_time_ = ros::Time::now();
 			tgcam(camaras_sol);
-			actions_todo.update_toggle_cam=false;
+			ROS_INFO("Now, I'm not turning.");
+		    }
 		}
-
-		// Reset ArDrone
-		if (actions_todo.update_reset){
-			ROS_INFO("Reset!");
-			std_msgs::Empty msg;
-			reset_pub.publish(msg);
-			actions_todo.update_reset=false;
+		if (waiting_ && ros::Time::now()-prev_time_>TIME_WAITING_){
+		    waiting_ = false;
+		    performing_ = true;
+		    t0_ = ros::Time::now();
+		    ROS_INFO("Now, I'm starting my movement.");
 		}
-
-
-		// State transitions
-		if(CURRENT_STATE_ != RECEIVED_STATE_){
-			std::string state;
-
-			switch(RECEIVED_STATE_){
-				case TAKINGOFF :
-					state = "Taking off";
-					if(!cam_){
-						tgcam(camaras_sol);
-					}
-					break;
-				case FLYING :
-					if(CURRENT_STATE_==TAKINGOFF){
-						turning_ = true;
-						prev_time_ = time_;
-					}
-					state = "Hovering";
-					break;
-				case LANDING :
-					state = "Landing";
-					break;
-				case LANDED :
-					state = "Landed";
-					break;
-			}
-			ROS_INFO("STATE: %s", state.c_str());
-
-			CURRENT_STATE_ = RECEIVED_STATE_;
+		if (performing_){
+		    double Ks [] = {0.3, 1, 1};
+		    double threshold=0.1, err;
+		    err = Pcontrol(final_distance, Ks, 3, twist_pub);
+		    
+		    if(err<threshold){
+			performing_=false;
+			waiting2_=true;
+			prev_time_=ros::Time::now();
+			ROS_INFO("Now, I have ended the execution of my movement.");
+		    }
 		}
-
-
-		if(CURRENT_STATE_==FLYING){
-			if (turning_){
-				double errX, errY, errZ, errYaw, Kx=1, Ky=1, Kz=1, Kyaw=1;
-				geometry_msgs::Twist msg;
-
-				errX = desired_distance_x_-_x_;
-				errY = desired_distance_y_-_y_;
-				errZ = desired_distance_z_-_z_;
-				errYaw = desired_angle_yaw_-yaw_;
-
-				msg.linear.y=Kx*errX;
-				msg.linear.x=Ky*errY;
-				msg.linear.z=Kz*errZ;
-				msg.angular.z=Kyaw*errYaw;
-				twist_pub.publish(msg);
-
-				if(ros::Time::now()-prev_time_>TIME_TURNING_){
-					turning_  = false;
-					waiting_ = true;
-					prev_time_ = ros::Time::now();
-					tgcam(camaras_sol);
-					ROS_INFO("Now, I'm not turning.");
-				}
-			}
-			if (waiting_ && ros::Time::now()-prev_time_>TIME_WAITING_){
-				waiting_ = false;
-				performing_ = true;
-				t0_ = ros::Time::now();
-				ROS_INFO("Now, I'm starting my movement.");
-			}
-			if (performing_){
-				double errX, errY, errZ, errYaw, Kx=0.3, Ky=1, Kz=1, threshold=0.1;
-				geometry_msgs::Twist msg;
-
-				errX = final_distance_x_-_x_;
-				errY = final_distance_y_-_y_;
-				errZ = final_distance_z_-_z_;
-
-				msg.linear.x=Kx*errX;
-				msg.linear.y=Ky*errY;
-				msg.linear.z=Kz*errZ;
-				
-				twist_pub.publish(msg);
-				//ROS_INFO("Error: %f\n", errX);
-
-				if(errX<threshold){
-					performing_=false;
-					waiting2_=true;
-					prev_time_=ros::Time::now();
-					ROS_INFO("Now, I have ended the execution of my movement.");
-				}
-			}
-			if (waiting2_ && ros::Time::now()-prev_time_>TIME_WAITING2_){
-				waiting2_=false;
-				std_msgs::Empty msgL;
-				Land_pub.publish(msgL);
-				Land_pub.publish(msgL);
-			}
+		if (waiting2_ && ros::Time::now()-prev_time_>TIME_WAITING2_){
+		    waiting2_=false;
+		    std_msgs::Empty msgL;
+		    Land_pub.publish(msgL);
+		    Land_pub.publish(msgL);
 		}
-		ros::spinOnce();
-		loop_rate.sleep();
+	    }
+	    ros::spinOnce();
+	    loop_rate.sleep();
 	}
 
 
